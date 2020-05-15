4fc82042aca9aed2e3b5ffeabefda97b1c2ce46c
Cleison Correia de Amorim <cleison.c.amorim@accenture.com>
2020-05-14 23:41:49 -0300

    Adjusting setup and requirements

--- snn-sl/setup.py
+++ snn-sl/setup.py
@@ -1,5 +1,14 @@
 from setuptools import setup
 from setuptools import find_packages
+import os
+
+requirement_file = os.path.dirname(
+    os.path.realpath(__file__)) + '/requirements.txt'
+install_requires = []
+
+if os.path.isfile(requirement_file):
+    with open(requirement_file) as f:
+        install_requires = f.read().splitlines()
 
 setup(
     name='snn-sl',
@@ -7,12 +16,8 @@ setup(
     author='Cleison C. Amorim',
     author_email='cca5@cin.ufpe.br',
     license='GPL-3.0',
-    install_requires=[
-        'numpy>=1.9.1', 'scipy>=0.14', 'keras>=2.3.1', 'tensorflow>=2.1.0',
-        'networkx>=2.4'
-    ],
+    install_requires=install_requires,
     extras_require={
-        'visualize': ['matplotlib>=3.1.3'],
         'tests': ['pytest', 'requests', 'markdown']
     },
     packages=find_packages())
